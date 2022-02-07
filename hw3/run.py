ef3c241a02448fb3319f148772da972aed99c98b
Matthew Ho <msho@ucsb.edu>
2022-02-07 12:45:11 -0800

    skeleton

---
+++ hw3/run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+# import the required packages here
+
+def run(Xtrain_file, Ytrain_file, test_data_file, pred_file):
+	'''The function to run your ML algorithm on given datasets, generate the predictions and save them into the provided file path
+	
+	Parameters
+	----------
+	Xtrain_file: string
+		the path to Xtrain csv file
+	Ytrain_file: string
+		the path to Ytrain csv file
+	test_data_file: string
+		the path to test data csv file
+	pred_file: string
+		the prediction file to be saved by your code. You have to save your predictions into this file path following the same format of Ytrain_file
+	'''
+
+	## your implementation here
+	# read data from Xtrain_file, Ytrain_file and test_data_file
+
+	# your algorithm
+
+	# save your predictions into the file pred_file
+
+
+# define other functions here
